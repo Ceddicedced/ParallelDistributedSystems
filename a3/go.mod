1d224e0ee4710ba1049c506c4468a39d98482a6f
Cedric Busacker <cbusacker@hostzero.com>
2023-12-27 20:36:49 +0100

    # Add A3

---
+++ a3/go.mod
@@ -0,0 +1,3 @@
+module busacker.dev/a3
+
+go 1.21.3
---
+++ a3/traffic_lights.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// Enumerations for CardinalDirection and Colour.
+type CardinalDirection int
+type Colour int
+type Axis int
+
+const (
+	North CardinalDirection = iota
+	East
+	South
+	West
+)
+
+const (
+	NorthSouth Axis = iota
+	EastWest
+)
+
+const (
+	Red Colour = iota
+	Green
+	Yellow
+)
+
+// Function to get the next Colour.
+func nextColour(c Colour) Colour {
+	return (c + 1) % 3
+}
+
+// TrafficLight goroutine.
+func TrafficLight(d CardinalDirection, axisFreeChan chan bool, redCountChan chan<- CardinalDirection, wg *sync.WaitGroup) {
+	defer wg.Done()
+	c := Red // Starting colour, initially set to Red.
+	for {
+		<-axisFreeChan // Wait for the axis to be free.
+		c = nextColour(c)
+		fmt.Printf("Traffic Light %v: %v\n", d, c)
+
+		if c == Red {
+			redCountChan <- d // Notify that this light is red.
+		} else {
+			axisFreeChan <- true // Signal that the axis is free.
+		}
+	}
+}
+
+func main() {
+	axisFreeChanNorthSouth := make(chan bool, 2)
+	axisFreeChanEastWest := make(chan bool, 2)
+	redCountChan := make(chan CardinalDirection)
+
+	// Counters for red lights.
+	redCountNorthSouth := 0
+	redCountEastWest := 0
+
+	currentAxis := NorthSouth // Start with North-South axis.
+	axisFreeChanNorthSouth <- true
+
+	var wg sync.WaitGroup
+	wg.Add(4)
+
+	// Start TrafficLight goroutines.
+	go TrafficLight(North, axisFreeChanNorthSouth, redCountChan, &wg)
+	go TrafficLight(East, axisFreeChanEastWest, redCountChan, &wg)
+	go TrafficLight(South, axisFreeChanNorthSouth, redCountChan, &wg)
+	go TrafficLight(West, axisFreeChanEastWest, redCountChan, &wg)
+
+	for {
+		select {
+		case direction := <-redCountChan:
+			if direction == North || direction == South {
+				redCountNorthSouth++
+				if redCountNorthSouth == 2 && currentAxis == NorthSouth {
+					currentAxis = EastWest
+					axisFreeChanEastWest <- true // Switch to East-West axis.
+					redCountNorthSouth = 0
+				} else {
+					axisFreeChanNorthSouth <- true
+				}
+			} else {
+				redCountEastWest++
+				if redCountEastWest == 2 && currentAxis == EastWest {
+					currentAxis = NorthSouth
+					axisFreeChanNorthSouth <- true // Switch to North-South axis.
+					redCountEastWest = 0
+				} else {
+					axisFreeChanEastWest <- true
+				}
+			}
+		}
+	}
+}
